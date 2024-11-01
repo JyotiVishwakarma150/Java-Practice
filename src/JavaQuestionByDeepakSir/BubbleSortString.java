44b417b080a3b158a45cd5cea378967368b11e85
Jyoti Vishwakarma <jyotivishwakarma2708@gmail.com>
2024-11-01 21:52:57 +0530

    BubbleSortString

---
+++ src/JavaQuestionByDeepakSir/BubbleSortString.java
@@ -0,0 +1,32 @@
+package JavaQuestionByDeepakSir;
+
+public class BubbleSortString {
+    static void sortingAlgo(String[] a){
+        for(int pass=0;pass<a.length;pass++){
+            String temp;
+            boolean b=false;
+            for(int i=0;i<a.length-1-pass;i++){
+                if(a[i].compareTo(a[i+1])>0){
+                    temp = a[i];
+                    a[i]=a[i+1];
+                    a[i+1]=temp;
+
+                    b=true;
+
+                }
+            }
+            if(b==false){
+                break;
+            }
+        }
+        System.out.println("final list");
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+       String[] arr = {"deepak","veronica","deepesh","amit","rahul"};
+        BubbleSortString.sortingAlgo(arr);
+    }
+}
