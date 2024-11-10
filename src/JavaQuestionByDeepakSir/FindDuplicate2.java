18cfb1683ff0eca98ff683c0233f1cc31dd16118
Jyoti Vishwakarma <jyotivishwakarma2708@gmail.com>
2024-11-10 13:25:19 +0530

    FindDuplicate2

---
+++ src/JavaQuestionByDeepakSir/FindDuplicate2.java
@@ -0,0 +1,17 @@
+package JavaQuestionByDeepakSir;
+
+public class FindDuplicate2 {
+    public static void main(String[] args) {
+        int[] arr = {40,30,70,90,60,10,30,70,50,30};
+        int count =0;
+        System.out.println("Duplicate elements are : ");
+        for(int i=0;i< arr.length-1;i++){
+            for(int j=i+1;j< arr.length;j++){
+                if(arr[i]==arr[j]){
+                    count = count+1;
+                    System.out.println(count + ". "+arr[i]);
+                }
+            }
+        }
+    }
+}
